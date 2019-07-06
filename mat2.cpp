cadb083385715d4105fb772baccbe3688a27a95e
Hardik Darak <kinghardik07@gmail.com>
2019-07-06 22:06:51 +0530

    identity added
    lots of good stuff going on

--- mat2.cpp
+++ mat2.cpp
@@ -12,8 +12,18 @@
 typedef std::vector<std::vector<double>> matrix;
 const int MATRIX_DIMENSION = 3;
 
+std::vector<int> matrixDimension(matrix input_matrix){
+    int row=input_matrix.size();
+    int column=input_matrix[0].size();
+    for(int i=1;i<row;i++){
+        if(input_matrix[i].size()!=column)
+            return {0,0};
+    }
+    return {row,column};
+}
+//try adding two seperate variables for row annd column and initialize them in matrixDimension function
 bool dimensionError(matrix input_matrix_1, matrix input_matrix_2){
-    if(input_matrix_1.size() == input_matrix_2.size())
+    if(matrixDimension(input_matrix_1) == matrixDimension(input_matrix_2))
         return false;
     else{
         std::cout << "Dimension error! Dimensions don't match!" << std::endl;
@@ -62,6 +72,20 @@ matrix matrixProduct(matrix input_matrix_1, matrix input_matrix_2){
     return {{0}};
 }
 
+matrix identityMatrix(int order){
+    matrix identity(order);
+    for(int i=0; i<order; i++){
+//        identity[i](order);
+        for(int j=0; j<order; j++){
+            if(i==j){
+                identity[i].push_back(1);
+                continue;
+            }
+            identity[i].push_back(0);
+        }
+    }
+    return identity;
+}
 void displayMatrix(matrix input_matrix){
     std::cout << "The matrix is: \n";
     for(int i=0; i<input_matrix.size(); i++){
@@ -75,9 +99,9 @@ void displayMatrix(matrix input_matrix){
 int main(){
     matrix mat_1 = {{1,2,3},{4,5,6},{7,8,9}};
     matrix mat_2 = {{1,0,3},{3,4,5},{2,3,4}};
-    matrix identityMatrix = {{1,0,0},{0,1,0},{0,0,1}};
+    //matrix identityMatrix = {{1,0,0},{0,1,0},{0,0,1}};
     //displayMatrix(identityMatrix);
     //displayMatrix(addMatrices(mat_1,mat_2));
     //displayMatrix(scalarMultiplication(-1,mat_1));
-
+    //displayMatrix(addMatrices(mat_1,identityMatrix(3)));
 }
